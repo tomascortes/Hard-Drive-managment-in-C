3347d770849d04f3679a6d6c9218c25141879c0b
matlop <milopez8@uc.cl>
2022-04-30 18:56:40 -0400

    feat(common): avance en bitmap

--- src/ssdfs/main.c
+++ src/ssdfs/main.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
-#define BUFSZ 16
 
-int main(int argc, char const *argv[])
+int main(int argc, char** const argv[])
 {
-  printf("Hello P1!\n");
+  printf("BITMAP P1!\n");
   
+  unsigned char buffer[256];
+  FILE *f = fopen("simdiskfilled.bin", "rb");
+
+  fread(buffer, sizeof(buffer),1,f);
+
+  printf("\n");
+  for(int i = 0; i<256; i++){
+    for (int j = 7; j >= 0; j--){
+      printf("%d", (buffer[i] & (1 << j)) >> j );
+    }
+  }
+  printf("\n");
+  /* TODO else read failed, empty file?? */
+
+  fclose(f);
+  return 0;
 }
