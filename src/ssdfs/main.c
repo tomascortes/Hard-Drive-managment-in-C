6ee6081356f39cd21ba5248b5ff5fffe3283df6d
Luis González <ljgonzalez@uc.cl>
2022-04-30 23:22:03 -0400

    📜 Documentación, comentarios y orden

--- src/ssdfs/main.c
+++ src/ssdfs/main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include "os_API.h"
 
-int main(int argc, char** const argv[])
-{
-  os_mount(argv[1], 5000);
+// FIXME: Second parameter of 'main' (argument array) must be type 'char **'
+int main(int argc, char** const argv[]) {
+    os_mount(argv[1], 5000);
 
-  os_bitmap(0);
+    os_bitmap(0);
 
-  return 0;
+    return 0;
 }
--- src/ssdfs/os_API.h
+++ src/ssdfs/os_API.h
@@ -3,5 +3,22 @@
 char global_diskname[1023];
 int global_P_E;
 
+// Funciones generales
 void os_mount(char* diskname, unsigned life);
 void os_bitmap(unsigned num);
+void os_lifemap(int lower, int upper);  // TODO: Pendiente
+int os_trim(unsigned limit);  // TODO: Pendiente
+void os_tree();  // TODO: Pendiente
+
+// Funciones de manejo de archivos
+int os_exists(char* filename);  // TODO: Pendiente
+osFile* os_open(char* filename, char mode);  // TODO: Pendiente
+int os_read(osFile* file_desc, void* buffer, int nbytes);
+int os_write(osFile* file_desc, void* buffer, int nbytes);  // TODO: Pendiente
+int os_close(osFile* file_desc);  // TODO: Pendiente
+int os_rm(char* filename);  // TODO: Pendiente
+int os_mkdir(char* path);  // TODO: Pendiente
+int os_rmdir(char* path);  // TODO: Pendiente
+int os_rmrfdir(char* path);  // TODO: Pendiente
+int os_unload(char* orig, char* dest);  // TODO: Pendiente
+int os_load(char* orig);  // TODO: Pendiente
