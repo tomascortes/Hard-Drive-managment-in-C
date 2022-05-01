b2171569657b37816188e8ce2c9f0737fad49cef
Luis González <ljgonzalez@uc.cl>
2022-05-01 12:10:56 -0400

    Un par de notas

--- src/ssdfs/os_file/os_file.h
+++ src/ssdfs/os_file/os_file.h
@@ -16,7 +16,16 @@
 
 #pragma once
 
+// XXX: Lo siento, no pude restringirme a las funciones que nos dijeron sin hacer un desastre.
+//  Puse estas para manejar el struct
+
 // Representación de archivos abiertos mediate struct
+// REVIEW: Alguien por favor revise si esto se puede combinar dentro de os_file
+//  con 'typedef struct osFile {...} osFile;' a pesar de referenciarlo dentro
+//  de sí mismo para reservar memoria
+struct os_file;
+typedef struct os_file osFile;
+
 struct os_file {
     char* name;  // Nombre del archivo
     char mode[2]; // r -> ReadOnly || {w,rw,wr,r+} -> ReadWrite
