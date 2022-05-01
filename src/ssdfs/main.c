525d2194eabbf4590bac1ee852840e20b1557cf0
unknown <fivillagran@uc.cl>
2022-05-01 10:35:37 -0400

    👥 Incorporo aportes de "unknown" desde main

--- src/ssdfs/main.c
+++ src/ssdfs/main.c
@@ -2,10 +2,15 @@
 #include "os_API.h"
 
 // FIXME: Second parameter of 'main' (argument array) must be type 'char **'
-int main(int argc, char** const argv[]) {
+int main(int argc, char** const argv[]) {// Montar el disco pasado por consola con life = 5000
     os_mount(argv[1], 5000);
 
-    os_bitmap(0);
+    os_bitmap(0); // Bitmap completo
+    os_bitmap(2); // Bitmap bloque N°2 (Tercer bloque)
+    os_bitmap(395); // Bitmap bloque N°395
+    os_bitmap(404404); // Bitmap bloque inexistente
+
+    os_tree();
 
     return 0;
 }
--- src/ssdfs/os_API.c
+++ src/ssdfs/os_API.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 #include "./os_API.h"
 
 // ===== API de ssdfs =====
@@ -17,6 +18,8 @@
  * La función debe poder ser llamada múltiples veces si se desea abrir
  * diferentes discos a lo largo de la ejecución de main.c.*/
 void os_mount(char* diskname, unsigned life) {
+    /* Crea una variable global con el nombre del archivo y otra con el
+     * valor de life */
     strcpy(global_diskname, diskname);
     global_P_E = life;
 }
@@ -25,45 +28,54 @@ void os_mount(char* diskname, unsigned life) {
  * Si num=0 se debe imprimir t */
 void os_bitmap(unsigned num) {
     printf("BITMAP P1!\n");
+
+    // Abro el archivo
     FILE *f = fopen(global_diskname, "rb");
 
-    unsigned char buffer[256];
-    fread(buffer, sizeof(buffer),1,f);
+    // El disco tiene 2048 bloques, por lo que para el bitmap necesitamos
+    // 2048 bits = 256 bytes
+    unsigned char buffer[256]; // Buffer para guardar los bytes
+    fread(buffer, sizeof(buffer), 1, f);
 
     if (num == 0) {
-        // Printear 't'
-        int fill = 0;
-        int free = 0;
+        int fill=0;
+        int free=0;
 
-        for (int i = 0; i<256; i++) {
+        for(int i = 0; i < 256; i++) {
             for (int j = 7; j >= 0; j--) {
-                int bit = (buffer[i] & (1 << j)) >> j;
+                int bit = (buffer[i] & (1 << j)) >> j; // Shift left para sacar el bit
                 printf("%d", bit );
-
-                if (bit) {
-                    fill++;
-
-                } else {
-                    free++;
-                }
+                bit ? fill++ : free++; // Se ve más cool así
             }
         }
 
         printf("\n");
-        printf("Bloques Ocupados: %d\nBloques Libres: %d\n", fill, free);
+        printf("Bloques Ocupados: %d\nBloques Libres: %d\n\n", fill, free);
 
-    } else if (num>=0 && num<=2048) {
-        // Printea bitmap para el bloque num.
-        printf("%d\n", num/8);
-        printf("%d\n", num%8);
-        printf("%d\n", (buffer[num/8] & (1 << (7-num%7)) >> num%8 ));
+    } else if (num > 0 && num <= 2048) {
+        // num/8 es el byte donde se encuentra el bit deseado
+        // num%8 es el offset del bit dentro de ese byte
+        printf("%d\n", (buffer[num/8] & 1 << (7-num%8)) >> (7-num%8));
 
+        // En el momento 15:35 de la cápsula P1 dice que esto hay que entregarlo
+        // aunque el argumento no sea 0
+
+        int fill=0;
+        int free=0;
+
+        for (int i = 0; i < 256; i++) {
+            for (int j = 7; j >= 0; j--) {
+                int bit = (buffer[i] & (1 << j)) >> j; // Shift left para sacar el bit
+                bit ? fill++ : free++; // Se ve más cool así
+            }
+        }
+
+        printf("Bloques Ocupados: %d\nBloques Libres: %d\n\n", fill, free);
     } else {
-        // num no pertenece al array (0..2047)
-        printf("%s\n", "SEGFAULT");
+        printf("%s\n\n", "SEGFAULT uwu");
     }
-    
-    fclose(f);
+
+    fclose(f); // Evitamos leaks
 }
 
 /* Imprime el estado P/E de las páginas desde lower y upper-1.
@@ -86,8 +98,44 @@ int os_trim(unsigned limit) {  // TODO: Pendiente
 }
 
 /* Función para imprimir el árbol de directorios y archivos del sistema, a partir del directorio base. */
-void os_tree() {  // TODO: Pendiente
-    return;
+void os_tree(){
+    // Abro el archivo
+    FILE *f = fopen(global_diskname, "rb");
+
+    // Me muevo 3 MiB, para llegar al bloque N°3, de directorio.
+    fseek(f, 3145728, SEEK_SET);
+
+    // La entrada 1672 tiene un archivo
+    // Son 32768 entradas en el bloque de directorio
+    for (int i = 0; i < 32768; i++) {
+        unsigned char buffer[32]; // Buffer para guardar los bytes de una entrada
+        fread(buffer, sizeof(buffer), 1, f); // Leo una entrada
+
+        if(buffer[0] == 1){ // directorio:
+            printf("Primer byte entrada %i: %i\n", i, buffer[0]);
+            // Printear nombre del archivo
+
+            for (int j = 5; j < 32; j++) {
+                printf("%c", buffer[j]);
+            }
+
+            printf("\n");
+            int puntero = buffer[1]; // Pesco los bytes 1-4
+            printf("Puntero: %i\n", puntero);
+            // Pendiente
+            //fseek(f, puntero*4096, SEEK_SET); // Cada pág tiene 4096 bytes
+
+        } else if(buffer[0] == 3){ // archivo:
+            printf("Primer byte entrada %i: %i\n", i, buffer[0]);
+            // Printear nombre del archivo
+            for (int j = 5; j < 32; j++) {
+                printf("%c", buffer[j]);
+            }
+            printf("\n");
+        }
+    }
+
+    fclose(f); // Evitamos leaks
 }
 
 
@@ -170,4 +218,31 @@ int os_load(char* orig) {  // TODO: Pendiente
     return 0;
 }
 
+// Temporal ----- Esta función es para testear
+// Tira los nombres de todo lo que hay en el disco
+void print_names(){
+    // Abro el archivo
+    FILE *f = fopen(global_diskname, "rb");
 
+    // Me muevo 3 MiB, para llegar al bloque N°3, del directorio base.
+    fseek(f, 3145728, SEEK_SET);
+
+    // root está en el bloque 3 por convención, por lo que si
+    // hubiese que moverlo para que no se pudra, se perdería para siempre
+
+    // (La entrada 1672 tiene un archivo)
+    // Son 32768 entradas en el bloque de directorio
+    for(int i = 0; i < 32768; i++){
+        unsigned char buffer[32]; // Buffer para guardar los bytes de una entrada
+        fread(buffer, sizeof(buffer), 1, f); // Leo una entrada
+        if(buffer[0]){ // Si hay archivo o directorio:
+            printf("Primer byte entrada %i: %i\n", i, buffer[0]);
+            // Printear nombre del archivo
+            for(int j = 5; j < 32; j++){
+                printf("%c", buffer[j]);
+            }
+            printf("\n");
+        }
+    }
+    fclose(f); // Evitamos leaks
+}
