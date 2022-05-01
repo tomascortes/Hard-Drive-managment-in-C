bb469ee4ef4e590beba0e05d7fca08dc0dbceb9a
Luis González <ljgonzalez@uc.cl>
2022-04-30 23:22:03 -0400

    Maña mía + sangría

--- src/ssdfs/main.c
+++ src/ssdfs/main.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include "os_API.h"
 
-// FIXME: Second parameter of 'main' (argument array) must be type 'char **'
 int main(int argc, char** const argv[]) {
-  os_mount(argv[1], 5000);
+    os_mount(argv[1], 5000);
 
-  os_bitmap(0);
+    os_bitmap(0);
 
-  return 0;
+    return 0;
 }
