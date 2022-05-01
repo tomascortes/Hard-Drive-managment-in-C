0868d4f89f2abf0360a683a1b024c41848a7e2e6
unknown <fivillagran@uc.cl>
2022-05-01 05:43:34 -0400

    readme y os_tree en proceso

--- src/ssdfs/main.c
+++ src/ssdfs/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "os_API.h"
 
+
 int main(int argc, char** const argv[])
 {
   // Montar el disco pasado por consola con life = 5000
@@ -10,6 +11,8 @@ int main(int argc, char** const argv[])
   os_bitmap(2); // Bitmap bloque N°2 (Tercer bloque)
   os_bitmap(395); // Bitmap bloque N°395
   os_bitmap(404404); // Bitmap bloque inexistente
+
+  os_tree();
 
   return 0;
 }
--- src/ssdfs/os_API.c
+++ src/ssdfs/os_API.c
@@ -53,5 +53,73 @@ void os_bitmap(unsigned num){
     printf("%s\n\n", "SEGFAULT uwu");
   }
   
-  fclose(f);
+  fclose(f); // Evitamos leaks
+}
+
+void os_tree(){
+  // Abro el archivo
+  FILE *f = fopen(global_diskname, "rb");
+
+  // Me muevo 3 MiB, para llegar al bloque N°3, de directorio.
+  fseek(f, 3145728, SEEK_SET);
+
+  // La entrada 1672 tiene un archivo
+  // Son 32768 entradas en el bloque de directorio
+  for(int i = 0; i < 32768; i++){
+    unsigned char buffer[32]; // Buffer para guardar los bytes de una entrada
+    fread(buffer, sizeof(buffer), 1, f); // Leo una entrada
+    if(buffer[0] == 1){ // directorio:
+      printf("Primer byte entrada %i: %i\n", i, buffer[0]);
+      // Printear nombre del archivo
+      for(int j = 5; j < 32; j++){
+        printf("%c", buffer[j]);
+      }
+      printf("\n");
+      int puntero = buffer[1]; // Pesco los bytes 1-4
+      printf("Puntero: %i\n", puntero);
+      // Pendiente
+      //fseek(f, puntero*4096, SEEK_SET); // Cada pág tiene 4096 bytes
+
+
+    } else if(buffer[0] == 3){ // archivo:
+      printf("Primer byte entrada %i: %i\n", i, buffer[0]);
+      // Printear nombre del archivo
+      for(int j = 5; j < 32; j++){
+        printf("%c", buffer[j]);
+      }
+      printf("\n");
+    }
+  }
+
+
+  fclose(f); // Evitamos leaks
+}
+
+// Temporal ----- Esta función es para testear
+// Tira los nombres de todo lo que hay en el disco
+void print_names(){
+  // Abro el archivo
+  FILE *f = fopen(global_diskname, "rb");
+
+  // Me muevo 3 MiB, para llegar al bloque N°3, del directorio base.
+  fseek(f, 3145728, SEEK_SET);
+
+  // root está en el bloque 3 por convención, por lo que si 
+  // hubiese que moverlo para que no se pudra, se perdería para siempre
+
+  // (La entrada 1672 tiene un archivo)
+  // Son 32768 entradas en el bloque de directorio
+  for(int i = 0; i < 32768; i++){
+    unsigned char buffer[32]; // Buffer para guardar los bytes de una entrada
+    fread(buffer, sizeof(buffer), 1, f); // Leo una entrada
+    if(buffer[0]){ // Si hay archivo o directorio:
+      printf("Primer byte entrada %i: %i\n", i, buffer[0]);
+      // Printear nombre del archivo
+      for(int j = 5; j < 32; j++){
+        printf("%c", buffer[j]);
+      }
+      printf("\n"); 
+    }
+  }
+  fclose(f); // Evitamos leaks
 }
