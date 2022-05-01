3411dab166bd77c4e27d67a5c9c2e35b6b52f015
matlop <milopez8@uc.cl>
2022-05-01 14:03:29 -0400

    feat(common): bitmap completo

--- src/ssdfs/main.c
+++ src/ssdfs/main.c
@@ -1,33 +1,20 @@
-/* +===================================+=================================+
- * |     P01 IIC2333      |    DCCegmentation Fault   |    2022-05-04    |
- * +==================+================+===================+=============+
- * |      Nombre      |  Usr. GitHub   |      Correo       | Nro. Alumno |
- * +==================+================+===================+=============+
- * | Matías López     | MatLop1        | milopez8@uc.cl    | 17210674    |
- * +------------------+----------------+-------------------+-------------+
- * | Tomás Cortés     | ticortes       | ticortez@uc.cl    | 17640849    |
- * +------------------+----------------+-------------------+-------------+
- * | Felipe Villagrán | BibarelUsedFly | fivillagran@uc.cl | 16638689    |
- * +------------------+----------------+-------------------+-------------+
- * | Marcelo Bernal   | msbernal       | msbernal@uc.cl    | 1763671J    |
- * +------------------+----------------+-------------------+-------------+
- * | Luis González    | ljgonzalez1    | ljgonzalez@uc.cl  | 16625439    |
- * +------------------+----------------+-------------------+-------------+ */
-
 #include <stdio.h>
 #include "os_API.h"
 
-// FIXME: "Second parameter of 'main' (argument array) must be of type 'char **'"
-//  Ni idea cómo se supone que uno corrija esto
-int main(int argc, char** const argv[]) {// Montar el disco pasado por consola con life = 5000
-    os_mount(argv[1], 5000);
 
-    os_bitmap(0); // Bitmap completo
-    os_bitmap(2); // Bitmap bloque N°2 (Tercer bloque)
-    os_bitmap(395); // Bitmap bloque N°395
-    os_bitmap(404404); // Bitmap bloque inexistente
+int main(int argc, char* const argv[])
+{
+  // Montar el disco pasado por consola con life = 5000
+  os_mount(argv[1], 5000);
 
-    os_tree();
+  os_bitmap(0); // Bitmap completo
+  os_bitmap(2); // Bitmap bloque N°2 (Se espera 1)
+  os_bitmap(7); // Bitmap bloque N°7 (Se espera 0)
+  os_bitmap(8); // Bitmap bloque N°8 (Se espera 1)
+  os_bitmap(395); // Bitmap bloque N°395 (Se espera 0)
+  os_bitmap(404404); // Bitmap bloque inexistente (Se espera SEGFAULT)
 
-    return 0;
+  os_tree();
+
+  return 0;
 }
--- src/ssdfs/os_API.h
+++ src/ssdfs/os_API.h
@@ -1,44 +1,9 @@
-/* +===================================+=================================+
- * |     P01 IIC2333      |    DCCegmentation Fault   |    2022-05-04    |
- * +==================+================+===================+=============+
- * |      Nombre      |  Usr. GitHub   |      Correo       | Nro. Alumno |
- * +==================+================+===================+=============+
- * | Matías López     | MatLop1        | milopez8@uc.cl    | 17210674    |
- * +------------------+----------------+-------------------+-------------+
- * | Tomás Cortés     | ticortes       | ticortez@uc.cl    | 17640849    |
- * +------------------+----------------+-------------------+-------------+
- * | Felipe Villagrán | BibarelUsedFly | fivillagran@uc.cl | 16638689    |
- * +------------------+----------------+-------------------+-------------+
- * | Marcelo Bernal   | msbernal       | msbernal@uc.cl    | 1763671J    |
- * +------------------+----------------+-------------------+-------------+
- * | Luis González    | ljgonzalez1    | ljgonzalez@uc.cl  | 16625439    |
- * +------------------+----------------+-------------------+-------------+ */
-
 #pragma once
-
-#include "./os_file/os_file.h"  // NOTE: Trabajando en esto
 
 char global_diskname[1023];
 int global_P_E;
 
-// Funciones generales
 void os_mount(char* diskname, unsigned life);
 void os_bitmap(unsigned num);
-void os_lifemap(int lower, int upper);  // TODO: Pendiente
-int os_trim(unsigned limit);  // TODO: Pendiente
 void os_tree();
-
-// Funciones de manejo de archivos
-int os_exists(char* filename);  // TODO: Pendiente
-osFile* os_open(char* filename, char mode);  // TODO: Pendiente
-int os_read(osFile* file_desc, void* buffer, int nbytes);  // NOTE: Trabajando en esto
-int os_write(osFile* file_desc, void* buffer, int nbytes);  // TODO: Pendiente
-int os_close(osFile* file_desc);  // TODO: Pendiente
-int os_rm(char* filename);  // TODO: Pendiente
-int os_mkdir(char* path);  // TODO: Pendiente
-int os_rmdir(char* path);  // TODO: Pendiente
-int os_rmrfdir(char* path);  // TODO: Pendiente
-int os_unload(char* orig, char* dest);  // TODO: Pendiente
-int os_load(char* orig);  // TODO: Pendiente
-
 void print_names();
