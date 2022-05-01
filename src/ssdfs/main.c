9f6c85de0a3c846b15170a2cf6245bccf83bfa89
matlop <milopez8@uc.cl>
2022-05-01 15:19:18 -0400

    feat(common): formato general y arreglos bitmap

--- src/ssdfs/main.c
+++ src/ssdfs/main.c
@@ -1,11 +1,26 @@
+/* +===================================+=================================+
+ * |     P01 IIC2333      |    DCCegmentation Fault   |    2022-05-04    |
+ * +==================+================+===================+=============+
+ * |      Nombre      |  Usr. GitHub   |      Correo       | Nro. Alumno |
+ * +==================+================+===================+=============+
+ * | Matías López     | MatLop1        | milopez8@uc.cl    | 17210674    |
+ * +------------------+----------------+-------------------+-------------+
+ * | Tomás Cortés     | ticortes       | ticortez@uc.cl    | 17640849    |
+ * +------------------+----------------+-------------------+-------------+
+ * | Felipe Villagrán | BibarelUsedFly | fivillagran@uc.cl | 16638689    |
+ * +------------------+----------------+-------------------+-------------+
+ * | Marcelo Bernal   | msbernal       | msbernal@uc.cl    | 1763671J    |
+ * +------------------+----------------+-------------------+-------------+
+ * | Luis González    | ljgonzalez1    | ljgonzalez@uc.cl  | 16625439    |
+ * +------------------+----------------+-------------------+-------------+ */
+
 #include <stdio.h>
 #include "os_API.h"
 
 
-int main(int argc, char* const argv[])
-{
+int main (int argc, char* const argv[]) {
   // Montar el disco pasado por consola con life = 5000
-  os_mount(argv[1], 5000);
+    os_mount(argv[1], 5000);
 
   os_bitmap(0); // Bitmap completo
   os_bitmap(8); // Bitmap bloque N°8 (Se espera 1)
@@ -13,7 +28,7 @@ int main(int argc, char* const argv[])
   os_bitmap(2048); // Bitmap bloque inexistente (Se espera SEGFAULT)
   printf("\n");
 
-  os_tree();
+    os_tree();
 
-  return 0;
+    return 0;
 }
