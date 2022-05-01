92b56913aeecc5d58fa2287c57577d74458afe91
Luis González <ljgonzalez@uc.cl>
2022-04-30 23:29:47 -0400

    Nota sobre error

--- src/ssdfs/main.c
+++ src/ssdfs/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include "os_API.h"
 
+// FIXME: Second parameter of 'main' (argument array) must be type 'char **'
 int main(int argc, char** const argv[]) {
     os_mount(argv[1], 5000);
 
