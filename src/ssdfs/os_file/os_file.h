28f563178f94566c6954ca4cc6289d159a348972
Luis González <ljgonzalez@uc.cl>
2022-05-02 17:19:12 -0400

    Nuevas variables

--- src/ssdfs/os_file/os_file.h
+++ src/ssdfs/os_file/os_file.h
@@ -20,32 +20,35 @@
 
 // Representación de archivos abiertos mediate struct
 typedef struct osFile {
-    // REVIEW: Revisar que el nombre de tamaño indefinido no interfiera con
-    //  malloc(sizeof(osFile))
-    char* name;  // Nombre del archivo
+    char *name;  // Nombre del archivo
     // Puse 2 caracteres para que sea un poco más a prueba de errores
     char mode[2]; // r -> ReadOnly || w-> WriteOnly || {rw,wr,r+} -> ReadWrite || N -> Null
 
-    int starting_pos; // Posición donde inicia el bloque en el que parte el archivo.
+    char* disk; // Puntero al disco para hacer las lecturas.
 
-    int start_block;  // Bloque donde comienza el archivo
-    int length_bytes;  // Largo del archivo en bytes
-    int end_block; // Último bloque que alcanza a ocupar
+    unsigned int current_plane; // Número de plano en el que se encuentra el archivo.
+    // plane --> {0..1}
+    unsigned int current_block; // Número de bloque en el que se encuentra el archivo.
+    // block --> {0..1023}
+    // cada pag tiene 2048 celdas
+    // cada celda son 16b
 
-    int current_block; // Bloque actual
-    int current_page; // Página actual del bloque actual
-    int current_pos; // Posición actual de la página actual
+    int length; // Largo en bytes del archivo
 
-    void* file;  // Apunta al archivo. TODO
-    unsigned int block;
-    unsigned int page;
+    unsigned int current_page; // Página actual
+    // page --> {0..256}
+    unsigned int current_pos; // Posición actual dentro de la página actual
+    // pos --> {0..4096}
+
 } osFile;
 
-osFile* osFile_new(char* name);
+osFile* osFile_new(char* name, char* disk_pointer);
+
 osFile* osFile_set_mode(osFile* self, char mode[2]);
 osFile* osFile_set_location(osFile* self,
-                            int start_block,
-                            int end_block,
+                            unsigned int plane,
+                            unsigned int block,
+                            // Bloque de índice dice el tamaño del archivo
                             int length_bytes);
 osFile* osFile_offset_pointer(osFile* self, int offset);
 void osFile_destroy(osFile* self);
