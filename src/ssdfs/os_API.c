aa5f346cdcda90ccc2d7ebc347ef69a12c099bdc
Luis González <ljgonzalez@uc.cl>
2022-05-04 02:20:44 -0400

    Formato y detalles

--- src/ssdfs/os_API.c
+++ src/ssdfs/os_API.c
@@ -33,7 +33,7 @@ void os_mount(char* diskname, unsigned life) {
     /* Crea una variable global con el nombre del archivo y otra con el
      * valor de life */
     strcpy(global_diskname, diskname);
-    //// FIXME: "Narrowing conversion from 'unsigned int' to signed type 'int'
+    //// WARN: "Narrowing conversion from 'unsigned int' to signed type 'int'
     ////  is implementation-defined"
     ////  --------------------------------------------------------
     ////  Tal vez algún check o casteo lo arregla?
@@ -237,6 +237,7 @@ void os_tree(){
             for (int k = 0; k < depth; k++) { // Desplazar depth a la derecha
                 printf("| ");
             }
+
             for (int j = 5; j < DIR_ENTRY_SIZE; j++) { // Printear nombre del directorio
                 printf("%c", buffer[j]);
             }
@@ -249,8 +250,9 @@ void os_tree(){
             ////  --------------------------------------------------------
             ////  Supongo que no definir una función dentro de otra solucionaría el
             ////  problema
-            directree(puntero, depth); // Función recursiva para leer
-                                          // dentro del directorio
+            // Función recursiva para leer
+            // dentro del directorio
+            directree(puntero, depth);
             depth--; // Vuelvo a la profundidad anterior
         } 
         
@@ -272,7 +274,7 @@ void os_tree(){
 // ----- Funciones de manejo de archivos -----
 /* Permite revisar si un archivo existe o no. Retorna 1 en caso de que exista, 0 de caso
  * contrario. */
-int os_exists(char* filename) {  // TODO: Pendiente
+int os_exists(char* filename) {
     // Defino la verión recursiva de la función acá adentro
     // para cumplir con las reglas de no ofrecer más funciones en la API
     //// FIXME: Me tira error.
@@ -301,26 +303,34 @@ int os_exists(char* filename) {  // TODO: Pendiente
                 char path2[100]; // path actual
                 char aux[2]; // variable para concatenar char
                 strcpy(path2, path); // Copiar strings
+
                 for (int j = 5; j < DIR_ENTRY_SIZE; j++) { // Printear nombre del archivo
                     aux[1] = '\0';
+                    //// WARN: Se está tirando un "unsign char" a "char"
                     aux[0] = buffer[j];
                     strcat(path2, aux); // Concatenar char
                 }
+
                 printf("Path: %s\n", path2);
+
                 if (strcmp(path2, filename) == 0) { // compara con filename
                     fclose(f2); // Evitamos leaks
                     return 1;
                 }
             }
+
             else if(buffer[0] == 1) { // Directorio
                 char path2[100]; // path actual
                 char aux[2]; // variable para concatenar char
                 strcpy(path2, path); // Copiar strings
+
                 for (int j = 5; j < DIR_ENTRY_SIZE; j++) { // Printear nombre del directorio
                     aux[1] = '\0';
+                    //// WARN: Se está tirando un "unsign char" a "char"
                     aux[0] = buffer[j];
                     strcat(path2, aux); // Concatenar char
                 }
+
                 strcat(path, "/"); // Concatenar nuevo directorio
                 int puntero = buffer[1]; // Pesco los bytes 1-4
                 //// FIXME: Me tira error.
@@ -330,8 +340,9 @@ int os_exists(char* filename) {  // TODO: Pendiente
                 ////  problema
                 if (directreen(puntero, filename, path2)){// Función recursiva para leer
                     fclose(f2); // Evitamos leaks
+
                     return 1;
-                };
+                }
             }
         }
 
@@ -352,11 +363,13 @@ int os_exists(char* filename) {  // TODO: Pendiente
         unsigned char buffer[DIR_ENTRY_SIZE];
         // Buffer para guardar los bytes de una entrada
         fread(buffer, sizeof(buffer), 1, f); // Leo una entrada
+
         if(buffer[0] == 3){ // archivo:
             char path[100] = "/"; // path inicial
             char aux[2]; // variable para concatenar char
             for (int j = 5; j < DIR_ENTRY_SIZE; j++) { // Printear nombre del archivo
                 aux[1] = '\0';
+                //// WARN: Se está tirando un "unsign char" a "char"
                 aux[0] = buffer[j];
                 strcat(path, aux); // Concatenar char
             }
@@ -370,28 +383,35 @@ int os_exists(char* filename) {  // TODO: Pendiente
         else if (buffer[0] == 1) { // directorio:
             char path[100] = "/"; // path inicial
             char aux[2]; // variable para concatenar char
+
             for (int j = 5; j < DIR_ENTRY_SIZE; j++) { // Printear nombre del directorio
                 aux[1] = '\0';
+                //// WARN: Se está tirando un "unsign char" a "char"
                 aux[0] = buffer[j];
                 strcat(path, aux); // Concatenar char
             }
+
             strcat(path, "/");
             int puntero = buffer[1]; // Pesco los bytes 1-4
+
             //// FIXME: Me tira error.
             ////  Hace referencia a una función que marca como indefinida.
             ////  --------------------------------------------------------
             ////  Supongo que no definir una función dentro de otra solucionaría el
             ////  problema
-            if (directreen(puntero, filename, path)){// Función recursiva para leer
+            // Función recursiva para leer
+            if (directreen(puntero, filename, path)) {
                 fclose(f); // Evitamos leaks
                 printf("¡Esta!\n");
+
                 return 1;
-            };
+            }
         }
     }
 
     fclose(f); // Evitamos leaks
     printf("¡No Esta!\n");
+
     return 0;
 }
 
