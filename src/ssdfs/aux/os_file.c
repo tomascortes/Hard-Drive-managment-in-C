196a1117465b129de4b3eca3d55158802674e162
Luis González <ljgonzalez@uc.cl>
2022-05-09 05:30:44 -0400

    Cosas

--- src/ssdfs/aux/os_file.c
+++ src/ssdfs/aux/os_file.c
@@ -180,24 +180,55 @@ void fxExtra_liberar_mem_void(void* en_donde) {
 
 int fxExtra_nro_pagina_que_tengo_que_leer(osFile* file) {
     // TODO:
-    //  [ ] Ver dónde estoy parado
-    //  [ ] Calcular página según longitud
-    //  [ ] Meterme al índice y armar un array de bloques
-    //  [ ] Armar un array de página involucradas
-    //  [ ] Recorrer página por página y agregarla a un nuevo array si no está rotten
-    //  [ ] Mandar nro de página absoluta, incluyendo rotten
-    return 0;
+    //  [X] Ver dónde estoy parado
+    //  [X] Calcular página según longitud
+    //  [F] Meterme al índice y armar un array de bloques
+    //  [F] Armar un array de página involucradas
+    //  [F] Recorrer página por página y agregarla a un nuevo array si no está rotten
+    //  [-] Mandar nro de página absoluta, incluyendo rotten
+    //      [X] Mándo nro de pagina
+
+    long int tamanio = file->length;
+    int donde_voy_leyendo = file->bytes_loaded_count;
+    int mi_paginita = donde_voy_leyendo / PAGE_SIZE;
+
+    return mi_paginita;
 }
 
 void fxExtra_cargar_pagina_en_mem(osFile* file, char* dir_pagina, int nro_pag) {
     // TODO:
-    //  [ ] Ver dónde estoy parado
-    //  [ ] Calcular página según longitud
-    //  [ ] Meterme al índice y armar un array de bloques
+    //  [X] Ver dónde estoy parado
+    //  [X] Calcular página según longitud
+    //  [ ] Calcular nro de bloques que tiene
+    //  [-] Meterme al índice y armar un array de bloques
     //  [ ] Armar un array de página involucradas
     //  [ ] Recorrer página por página y agregarla a un nuevo array si no está rotten
     //  [ ] Avanzar pág por pág hasta la que necesito
     //  [ ] Cargar la que necesito en memoria
+
+    long int tamanio = file->length;
+    int donde_voy_leyendo = file->bytes_loaded_count;
+    int mi_paginita = nro_pag;
+
+    FILE* disco = fopen(global_diskname, "rb");
+
+    int tamanio_de_tamanio = sizeof(long int);
+    int offset = (file->block_index_number * BLOCK_SIZE) + tamanio_de_tamanio;
+
+    fseek(disco, offset, SEEK_SET);
+
+    // leer todo el bloque indice
+    fread(file->index_pointer, sizeof(file->index_pointer), 1, disco); // Leo una entrada
+
+    //encontrar length
+    // Si quiero acceder a los primeros 8 Byte es self->pointer
+    // Si quiero acceder a los 8 Byte siguientes al primer byte es (self->pointer + 1)
+    // Si suma en C corresponde al avance en punteros de los byte correspondientes a la variable
+    // +1 en char es 1 byte
+    // +1 en int son 4 byte
+    self->length = *(long int*)self->index_pointer;
+
+
 }
 
 bool quedan_bytes_por_leer(osFile* file) {
