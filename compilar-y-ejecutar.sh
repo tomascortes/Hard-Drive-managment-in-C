8fb4a63433d0b9d0b93aa4abe22af0a1aa3b2581
Luis González <ljgonzalez@uc.cl>
2022-05-09 17:56:05 -0400

    Algo así como que casi funciona

--- compilar-y-ejecutar.sh
+++ compilar-y-ejecutar.sh
@@ -22,5 +22,5 @@ echo -e "\033[33;1mHaciendo ejecutable\033[0m"
 chmod +x "./$FILE_NAME"
 
 echo -e "\033[33;1mCorriendo con valgrind\033[0m"
-valgrind --leak-check=full --show-leak-kinds=all "./$FILE_NAME" "$ARG1"
+valgrind --leak-check=full --show-leak-kinds=all -s "./$FILE_NAME" "$ARG1"
 echo -e "\033[33;1mFin\033[0m"
