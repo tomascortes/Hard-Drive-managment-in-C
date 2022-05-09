56c07d7336ce3c99b6b7d4f282b982a7e5e94311
Luis González <ljgonzalez@uc.cl>
2022-05-09 05:50:17 -0400

    F

--- src/ssdfs/aux/os_file.h
+++ src/ssdfs/aux/os_file.h
@@ -60,62 +60,6 @@ void osFile_set_mode(osFile* self, char mode);
 /// Settea la ubicación del puntero y largo del archivo
 void setup_from_disk(osFile* self, char* filename);
 
-/// Settea el archivo en el disco
-void put_on_disk(osFile* self, char* filename);
-
-// ----- File pointer -----
-/// Desplazo el puntero n espacios
-void osFile_offset_pointer(osFile* self, int offset);
-
-// ----- Offset -----
-/// Calcula el offset de una página del archivo en relación al inicio del disco
-long int osFile_calc_page_offset(osFile* self, int n_page);
-
-// ---- Page-R ----
-/// Calcula la página del bloque en la que se encuentra el puntero
-int osFile_get_current_page(osFile* self, const int* rotten_array_pointer);
-
-/// Resetea cuenta de bytes cargados
-void osFile_reset_bytes_count(osFile* self);
-
-/// Carga la página en la que se encuentra el puntero de lectura a memoria
-void osFile_load_pointer_page(osFile* self, const int* rotten_array_pointer);
-
-/// Cargo la página "n_page" del bloque en la dirección de memoria self->loaded_page
-void osFile_load_page(osFile* self, int n_page);
-
-/// Carga los datos del disco en memoria dado un offset
-void osFile_copy_page_data(osFile* self, long int offset);
-
-// ----- Page -----
-/// Si hay una página cargada, la libera
-void osFile_release_page_if_loaded(osFile* self);
-
-/// Libero la memoria de la página
-void osFile_release_page(osFile* self);
-
-// ---- Page-W ----
-/// Recibe el contenido de una página y lo guarda en memoria.
-/// Tiene que tener largo de una página
-void osFile_transfer_page(osFile* self, unsigned char content[PAGE_SIZE]);
-
-/// Escribe el contenido que tiene guardado en memoria en la página n_page del disco
-void osFile_write_page(osFile* self, int n_page);
-
-// ------ Mem -----
-/// Reservo memoria para una página y la asigno a self->loaded_page
-void osFile_reserve_page_mem(osFile* self);
-
-// ----- Data -----
-/// Carga datos desde la página cargada en memoria a un array.
-void osFile_load_data(osFile* self, int start, int end);
-
-/// Si hay una página cargada, la libera
-void osFile_release_data_if_loaded(osFile* self);
-
-/// Libero la memoria de la página
-void osFile_release_data(osFile* self);
-
 // ----- Clean -----
 /// Libera la memoria de todo lo asociado al struct. Luego libera la memoria del struct mismo.
 void osFile_destroy(osFile* self);
