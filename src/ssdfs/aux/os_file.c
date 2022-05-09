a63dddc8e76f8ce2ebc78f41f52bd953d031f1ff
Luis González <ljgonzalez@uc.cl>
2022-05-09 15:41:11 -0400

    Tan rapido como puedo...

--- src/ssdfs/aux/os_file.c
+++ src/ssdfs/aux/os_file.c
@@ -199,7 +199,8 @@ void fxExtra_cargar_pagina_en_mem(osFile* file, char* dir_pagina, int nro_pag) {
     // TODO:
     //  [X] Ver dónde estoy parado
     //  [X] Calcular página según longitud
-    //  [ ] Calcular nro de bloques que tiene
+    //  [X] Calcular nro de bloques que tiene
+    //  [X] Calcular nro de bloque en el que voy
     //  [-] Meterme al índice y armar un array de bloques
     //  [ ] Armar un array de página involucradas
     //  [ ] Recorrer página por página y agregarla a un nuevo array si no está rotten
@@ -220,13 +221,17 @@ void fxExtra_cargar_pagina_en_mem(osFile* file, char* dir_pagina, int nro_pag) {
     // leer todo el bloque indice
     fread(file->index_pointer, sizeof(file->index_pointer), 1, disco); // Leo una entrada
 
-    //encontrar length
-    // Si quiero acceder a los primeros 8 Byte es self->pointer
-    // Si quiero acceder a los 8 Byte siguientes al primer byte es (self->pointer + 1)
-    // Si suma en C corresponde al avance en punteros de los byte correspondientes a la variable
-    // +1 en char es 1 byte
-    // +1 en int son 4 byte
-    self->length = *(long int*)self->index_pointer;
+
+    // Bajo el supuesto de que por bloque no hay p´áginas rotten en el archivo porque no se puede escribir en un bloque on p´áginas rotten...
+    long int largo_del_archivo = (long int) file->length;
+    long int pre_cantidad_de_bloques_del_archivo = largo_del_archivo / BLOCK_SIZE;
+    int cantidad_de_bloques_del_archivo = (int) pre_cantidad_de_bloques_del_archivo;
+
+    long int pre_cantidad_de_bloques_del_archivo = file->by / BLOCK_SIZE;
+    int bloque_en_el_que_voy_leyendo =
+
+
+
 
 
 }
